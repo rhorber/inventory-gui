a8a911f1041575f510762a53b7166ed464926643
Raphael Horber <raphael.horber@gmail.com>
2019-11-25 22:30:48 +0100

    Migrated store from classic to modules mode

--- store/index.js
+++ store/index.js
@@ -1,28 +1,37 @@
-import Vuex from 'vuex'
-
-const createStore = () => {
-  return new Vuex.Store({
-    state: {
-      accessToken: null
-    },
-    mutations: {
-      setAccessToken(state, accessToken) {
-        state.accessToken = accessToken;
-      }
-    },
-    actions: {
-      loadAccessToken({commit}) {
-        const accessToken = window.localStorage.getItem('accessToken');
-        commit('setAccessToken', accessToken);
-        return Promise.resolve(accessToken);
-      },
-      saveAccessToken({commit}, accessToken) {
-        window.localStorage.setItem('accessToken', accessToken);
-        commit('setAccessToken', accessToken);
-        return Promise.resolve();
-      }
-    }
-  })
+// Initial state
+const state = function () {
+  return {
+    accessToken: null
+  };
 };
 
-export default createStore
+// Getters
+const getters = {};
+
+// Mutations (setters; synchronous operations, preferably on one state property)
+const mutations = {
+  setAccessToken(state, accessToken) {
+    state.accessToken = accessToken;
+  }
+};
+
+// Actions (asynchronous operations and/or multiple mutations)
+const actions = {
+  loadAccessToken({commit}) {
+    const accessToken = window.localStorage.getItem('accessToken');
+    commit('setAccessToken', accessToken);
+    return Promise.resolve(accessToken);
+  },
+  saveAccessToken({commit}, accessToken) {
+    window.localStorage.setItem('accessToken', accessToken);
+    commit('setAccessToken', accessToken);
+    return Promise.resolve();
+  }
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+};
